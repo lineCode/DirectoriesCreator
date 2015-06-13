dbe589c5298b18bd93368ec5c272bef291af6b0d
Vyacheslav Bezborodov <vyacheslav.bezborodov@gmail.com>
2015-06-13 12:50:34 +0500

    Fix bug

--- create_path.h
+++ create_path.h
@@ -5,9 +5,9 @@
 
 
 
-uint64_t create_path_v1(const char *path );
-uint64_t create_path_v2(const char *path );
-uint64_t create_path_v3(const char *path );
+uint64_t create_path_v1(const char *path);
+uint64_t create_path_v2(const char *path);
+uint64_t create_path_v3(const char *path);
 
 
 
--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 
 
 
-int main(int argc, const char *argv[])
+int main()
 {
-    const char* path_root_required = "C:\\Documents and Settings\\Administrator\\Application Data\\ComplexSystems\\Delta8\\Settings";
     const char* path = "C:\\My Cool Folder\\Administrator\\Application Data\\ComplexSystems\\Delta8\\Settings";
 
-    std::cout << create_path_v1(path_root_required) << "\n";
+    std::cout << create_path_v1(path) << "\n";
     std::cout << create_path_v2(path) << "\n";
     std::cout << create_path_v3(path) << "\n";
 
